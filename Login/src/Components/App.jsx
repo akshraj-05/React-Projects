605e979aaa68afa5c3d250f2a6f9354cc8411a8f
akshraj chavda <90257725+akshraj-05@users.noreply.github.com>
2022-10-06 22:54:44 +0530

    form and state add

--- Login/src/Components/App.jsx
+++ Login/src/Components/App.jsx
@@ -2,22 +2,50 @@ import React, { useState } from "react";
 
 
 function App() {
+
+    const [massage, setMassage] = useState("Hello");
     const [Color, setColor] = useState({ backgroundColor: "white" });
+    const [Name, setName] = useState("");
+    const [Heading, setHeading] = useState("");
     function over() {
         setColor(previousState => {
             return { ...previousState, backgroundColor: "black" }
-          });
+        });
     }
     function out() {
         setColor(previousState => {
             return { ...previousState, backgroundColor: "white" }
-          });
+        });
+    }
+    function handleMassage(event) {
+
+        setMassage("Submitted");
+        handleHeading();
+        event.preventDefault();
+    }
+    function handleHeading() {
+        setHeading(Name);
+    }
+    function handleNameChange(event) {
+
+        setName(event.target.value);
+
     }
     return (
         <div className="container">
-            <h1>Hello</h1>
-            <input type="text" placeholder="What's your name?" />
-            <button onMouseOver={over} onMouseOut={out} style={Color}>Submit</button>
+            <h1>{massage} {Heading}</h1>
+            <form onSubmit={handleMassage}>
+                <input type="text" onChange={handleNameChange} value={Name} placeholder="What's your name?" />
+                <button
+                    onMouseOver={over}
+                    onMouseOut={out}
+                    style={Color}
+                    type="submit"
+
+                >
+                    Submit
+                </button>
+            </form>
         </div>
     );
 }
